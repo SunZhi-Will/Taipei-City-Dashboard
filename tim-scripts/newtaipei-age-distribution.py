0c48259f59a92e2b6756baa2dfa165b3cf34d4a3
Tim Liu <tim_liu@htc.com>
2025-05-31 18:31:39 +0800

    老人分佈

--- tim-scripts/newtaipei-age-distribution.py
+++ tim-scripts/newtaipei-age-distribution.py
@@ -2,8 +2,8 @@ import csv
 import os
 from collections import defaultdict
 
-csv_file_path = os.path.expanduser('~/Downloads/新北市國中里鄰學區.csv')
-output_csv_path = os.path.expanduser('~/hackathon/Taipei-City-Dashboard/tim-scripts/新北市每區學區數量.csv')
+csv_file_path = os.path.expanduser('~/Downloads/新北市年齡分佈.csv')
+output_csv_path = os.path.expanduser('~/hackathon/Taipei-City-Dashboard/tim-scripts/新北市老人分佈.csv')
 
 # Dictionary to hold counts per district
 district_counts = defaultdict(int)
@@ -12,9 +12,12 @@ with open(csv_file_path, mode='r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     
     for row in reader:
-        district_field = row['district']  # 區域
-        if district_field != "":
-            district_counts[district_field] += 1
+        field1 = row['\ufefffield1']  # 年度 & 區域
+        parts = field1.split(" ")
+        if parts[0] == '2023年':
+            district = parts[1].split("-")[0]
+            if district != '新北市':
+                district_counts[district] = int(row['percent28'])
 
 # Write the results to a new CSV file
 with open(output_csv_path, mode='w', encoding='utf-8-sig', newline='') as outfile:
@@ -22,4 +25,5 @@ with open(output_csv_path, mode='w', encoding='utf-8-sig', newline='') as outfil
     writer.writerow(['district', 'count'])  # header
     for district, count in sorted(district_counts.items()):
         writer.writerow([district, count])
+
 
